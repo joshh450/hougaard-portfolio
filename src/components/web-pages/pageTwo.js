83f564bc6f1043240fbae90629656e59aa66c46f
joshh450 <joshh450@yahoo.com>
2018-10-20 17:34:49 -0600

    turned each image of example sites into a link

--- src/components/web-pages/pageTwo.js
+++ src/components/web-pages/pageTwo.js
@@ -13,19 +13,19 @@ class PageTwo extends Component {
             <div className="pageTwo__portfolio__examples">
               <h3>Sensuous Sandwich Project</h3>
               <p>Development team and I built a demo site for Sensuous Sandwich in one day. Click to visit!</p>
-              <img className="site_examples" src="/assets/images/sensuous-sandwich.png" />
+              <a href="https://sensuous-sandwichs.herokuapp.com" target="blank"><img className="site_examples" src="/assets/images/sensuous-sandwich.png" /></a>
             </div>
 
             <div className="pageTwo__portfolio__examples">
               <h3>Class Scheduler App</h3>
               <p>I created a course scheduler application using React and JavaScript. Click to visit!</p>
-              <img className="site_examples" src="/assets/images/course-scheduler.png" />
+              <a href="https://jh-course-scheduler.herokuapp.com" target="blank"><img className="site_examples" src="/assets/images/course-scheduler.png" /></a>
             </div>
 
             <div className="pageTwo__portfolio__examples">
               <h3>Mad Libs App</h3>
               <p>I created a Mad Libs game with a very clean UI using React and JavaScript. Click to visit!</p>
-              <img className="site_examples" src="/assets/images/mad-libs.png" />
+              <a href="https://jh-madlibs.herokuapp.com" target="blank"><img className="site_examples" src="/assets/images/mad-libs.png" /></a>
             </div>
 
           </div>
