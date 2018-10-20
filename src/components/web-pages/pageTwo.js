5ab9bdcddec317cd0b9ea434ae0b8b19f74f00ae
joshh450 <joshh450@yahoo.com>
2018-10-20 17:27:15 -0600

    added a variables page for duplicate code, tested different fonts and ultimately changed the site font

--- src/components/web-pages/pageTwo.js
+++ src/components/web-pages/pageTwo.js
@@ -6,25 +6,25 @@ class PageTwo extends Component {
       <div className='pageTwo'>
         <div className="pageTwo__parallax">
           <div className="pageTwo__title">
-            <h1 className="pageTwo__portfolio__header">Portfolio</h1>
+            <h1 className="pageTwo__portfolio__header">Projects and Experience</h1>
           </div>
           <div className="pageTwo__portfolio__info">
 
             <div className="pageTwo__portfolio__examples">
               <h3>Sensuous Sandwich Project</h3>
-              <p>Development team and I built a demo site for Sensuous Sandwich in one day.</p>
+              <p>Development team and I built a demo site for Sensuous Sandwich in one day. Click to visit!</p>
               <img className="site_examples" src="/assets/images/sensuous-sandwich.png" />
             </div>
 
             <div className="pageTwo__portfolio__examples">
               <h3>Class Scheduler App</h3>
-              <p>I created a class scheduler application using React and JavaScript.</p>
+              <p>I created a course scheduler application using React and JavaScript. Click to visit!</p>
               <img className="site_examples" src="/assets/images/course-scheduler.png" />
             </div>
 
             <div className="pageTwo__portfolio__examples">
               <h3>Mad Libs App</h3>
-              <p>I created a Mad Libs game with a very clean UI using React and JavaScript.</p>
+              <p>I created a Mad Libs game with a very clean UI using React and JavaScript. Click to visit!</p>
               <img className="site_examples" src="/assets/images/mad-libs.png" />
             </div>
 
