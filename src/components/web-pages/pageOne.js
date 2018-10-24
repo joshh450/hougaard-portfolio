90c07d90293f1fe33366b94e63391c3d8afdb91d
joshh450 <joshh450@yahoo.com>
2018-10-24 13:58:41 -0600

    added social media navlinks to the top page and styled the send button to work a little better with a smaller screen

--- src/components/web-pages/pageOne.js
+++ src/components/web-pages/pageOne.js
@@ -10,6 +10,12 @@ class PageOne extends Component {
                   <h1>Joshua Hougaard</h1>
                   <h2>React, Python and JavaScript Developer</h2>
               </div>
+              <div className="homepage-socialmedia__nav-links">
+                <i className="fab fa-linkedin-in"></i>
+                <i className="fab fa-github"></i>
+                <i className="fab fa-codepen"></i>
+                <i className="fab fa-facebook-f"></i>
+            </div>
             </div>
         </div>
       </div>
