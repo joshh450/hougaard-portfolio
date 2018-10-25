8d8ba598a9a3555a8df5b442e06311628193ad09
joshh450 <joshh450@yahoo.com>
2018-10-25 13:05:58 -0600

    Refactored the page two examples so that the link becomes clickable while hovering over the whole div and not just the image

--- src/components/web-pages/pageTwo.js
+++ src/components/web-pages/pageTwo.js
@@ -11,21 +11,27 @@ class PageTwo extends Component {
           <div className="pageTwo__portfolio__info">
 
             <div className="pageTwo__portfolio__examples">
-              <h3>Sensuous Sandwich Project</h3>
-              <p>Development team and I built a demo site for Sensuous Sandwich in one day. Click to visit!</p>
-              <a href="https://sensuous-sandwichs.herokuapp.com" target="blank"><img className="site_examples" src="/assets/images/sensuous-sandwich.png" /></a>
+              <a className="pageTwo__portfolio__examples__links" href="https://sensuous-sandwichs.herokuapp.com" target="blank">
+                <h3>Sensuous Sandwich Project</h3>
+                <p>Development team and I built a demo site for Sensuous Sandwich in one day. Click to visit!</p>
+                <img className="site_examples" src="/assets/images/sensuous-sandwich.png" />
+              </a>
             </div>
 
             <div className="pageTwo__portfolio__examples">
-              <h3>Class Scheduler App</h3>
-              <p>I created a course scheduler application using React and JavaScript. Click to visit!</p>
-              <a href="https://jh-course-scheduler.herokuapp.com" target="blank"><img className="site_examples" src="/assets/images/course-scheduler.png" /></a>
+              <a className="pageTwo__portfolio__examples__links" href="https://jh-course-scheduler.herokuapp.com" target="blank">
+                <h3>Class Scheduler App</h3>
+                <p>I created a course scheduler application using React and JavaScript. Click to visit!</p>
+                <img className="site_examples" src="/assets/images/course-scheduler.png" />
+              </a>
             </div>
 
             <div className="pageTwo__portfolio__examples">
-              <h3>Mad Libs App</h3>
-              <p>I created a Mad Libs game with a very clean UI using React and JavaScript. Click to visit!</p>
-              <a href="https://jh-madlibs.herokuapp.com" target="blank"><img className="site_examples" src="/assets/images/mad-libs.png" /></a>
+              <a className="pageTwo__portfolio__examples__links" href="https://jh-madlibs.herokuapp.com" target="blank">
+                <h3>Mad Libs App</h3>
+                <p>I created a Mad Libs game with a very clean UI using React and JavaScript. Click to visit!</p>
+                <img className="site_examples" src="/assets/images/mad-libs.png" />
+              </a>
             </div>
 
           </div>
